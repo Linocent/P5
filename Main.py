80a404123bc16316f70f3442e59d5e396adffcaf
Linocent <timotheelinocent@gmail.com>
2020-06-19 10:51:48 +0200

    Rename BDD file.

--- Main.py
+++ Main.py
@@ -1,1 +1,14 @@
-import request
+import requests as rq
+
+
+
+def test():
+    r = rq.get('https://fr.openfoodfacts.org/categorie/popcorn/1.json')
+    if r.status_code == rq.codes.ok:
+        """requeste = rq.request('GET', 'https://fr.openfoodfacts.org/categories')
+        print(requeste)"""
+        print(r.json())
+    # categorie = rq.loads(r.text)
+    # print(categorie.keys())
+
+test()
